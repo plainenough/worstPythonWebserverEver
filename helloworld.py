77f2643e8cb0ffb4e1f5e9377dc7e14053a13e8b
Derrick Walton <dwalton@Derricks-MacBook-Pro.local>
2016-01-07 10:49:55 -0700

    Basic hello world commit.

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+#print("Hello World!")
+
+message = "Hello World!"
+print(message)
+
+message = "Hello python programming world!"
+print(message)
+
+#Generate an error. 
+#print(mesage)
