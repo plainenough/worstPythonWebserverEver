936bfcaa9ee752809132af91b381bad4d014ea66
Derrick Walton <derrick.walton@ymail.com>
2016-01-07 11:25:39 -0700

    Added upper/lower.

--- strings.py
+++ strings.py
@@ -2,3 +2,9 @@
 #Working with strings. Chapter2.
 name ="derrick walton" 
 print(name.title())
+
+name ="Derrick Walton" 
+#Using the lower function is great for storing data.
+print(name.lower())
+print(name.upper())
+
