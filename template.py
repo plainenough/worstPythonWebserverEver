916f147ad101114ac604a7c9d09ddf66444e056b
Derrick Walton <derrick.walton@ymail.com>
2016-03-14 14:10:28 -0600

    Updating template file.

--- template.py
+++ template.py
@@ -1,5 +1,16 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-#Fflat template for python projects
+import sys
+import codecs
 
+#Flat template for python projects
 
+def main(error):
+    UTF8Writer = codecs.getwriter('utf8')
+    sys.stdout = UTF8Writer(sys.stdout)
+    #Do something here.
+    return 
+
+if __name__ == '__main__':
+    main()
