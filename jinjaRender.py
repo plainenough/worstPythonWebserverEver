e59361e72e2901568ea92025b105ba0b5664d5f7
Derrick <derrick.e.walton@gmail.com>
2018-12-13 16:21:45 -0700

    Add help document, rework jinjaRender function

--- jinjaRender.py
+++ jinjaRender.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
+'''This prgoam renders your jinja
+Usage:  python jinjaRender.py filename 
+Optional arguments:
+    -p --path /path/to/dir/
+    
+import jinjaRender
+jinjaRender.jinjaRender(filename, path)
+'''
 
 def main():
-    import jinja2
+    '''For command line use calls parser and prints return
+    '''
     args = parser()
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(args.path))
-    template = env.get_template(args.filename)
-    rtemplate = template.render()
-    print(rtemplate)
+    print(jinjaRender(args.filename, args.path))
+    return
 
 def parser():
+    '''This function just loads the argmunts by default the assumption 
+    is made that the target template exists in the programs working 
+    directory
+    '''
     import argparse
     parser = argparse.ArgumentParser(description='Jinja Renderer.')
     parser.add_argument("filename", help='File-name', type=str)
@@ -16,6 +27,15 @@ def parser():
             help="The absolute path to the directory your template resides in",
             default="./", type=str)
     return parser.parse_args()
+
+def jinjaRender(filename, path):
+    '''This function pulls a template from an absolute path and atempts to 
+    render it providing the results.
+    '''
+    import jinja2
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(path))
+    template = env.get_template(filename)
+    return template.render() 
 
 if __name__ == '__main__':
     main()
