a0cc8e69bf41ff1acffbd84a2d00457cf9159f49
Derrick <derrick.e.walton@gmail.com>
2018-12-19 10:16:26 -0700

    Change to pep8 compliant

--- jinjaRender.py
+++ jinjaRender.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 '''This program renders your jinja templates
-Usage:  python jinjaRender.py filename 
+Usage:  python jinjaRender.py filename
 Optional arguments:
     -p --path /path/to/dir/
-    
+
 import jinjaRender
 jinjaRender.jinjaRender(filename, path)
 '''
+
 
 def main():
     '''For command line use calls parser and prints return
@@ -15,27 +16,31 @@ def main():
     print(jinjaRender(args.filename, args.path))
     return
 
+
 def parser():
-    '''This function just loads the argmunts by default the assumption 
-    is made that the target template exists in the programs working 
+    '''This function just loads the argmunts by default the assumption
+    is made that the target template exists in the programs working
     directory
     '''
     import argparse
     parser = argparse.ArgumentParser(description='Jinja Renderer.')
     parser.add_argument("filename", help='File-name', type=str)
-    parser.add_argument('-p', "--path",
+    parser.add_argument(
+            '-p', "--path",
             help="The absolute path to the directory your template resides in",
             default="./", type=str)
     return parser.parse_args()
 
+
 def jinjaRender(filename, path):
-    '''This function pulls a template from an absolute path and atempts to 
+    '''This function pulls a template from an absolute path and atempts to
     render it providing the results.
     '''
     import jinja2
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(path))
     template = env.get_template(filename)
-    return template.render() 
+    return template.render()
+
 
 if __name__ == '__main__':
     main()
