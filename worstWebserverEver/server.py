4d64a358b24e9b99361de424ddc4509d999bbf7c
Derrick Walton <dwalton@jane.com>
2019-02-14 14:33:48 -0700

    add receive functionality

--- worstWebserverEver/server.py
+++ worstWebserverEver/server.py
@@ -6,7 +6,21 @@ def main():
     # TODO: add configuration import
     while True:
         (clientsocket, address) = serversocket.accept()
-        clientsocket.run()
+        # mySocket(clientsocket)
+        rawreq = clientsocket.recv(1024)
+        req = str(rawreq, "utf-8").split(' ')
+        method = req[0]
+        uri = req[1]
+        addr = "{0}:{1}".format(address[0], address[1])
+        print("Connection from: {0} - {1}  --  {2}".format(addr, method, uri))
+        response = """\
+HTTP/1.1 200 OK
+
+Hello, World!
+"""
+        clientsocket.sendall(str.encode(response))
+        clientsocket.close()
+
 
 
 def createSocket():
