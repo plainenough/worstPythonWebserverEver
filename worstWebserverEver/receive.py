f9aa25c458724fb89ad84e6e91cf54a97e7fb546
Derrick Walton <dwalton@jane.com>
2019-02-14 16:48:40 -0700

    add recieve.py index.html

---
+++ worstWebserverEver/receive.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+
+def main(rawreq):
+    # Request parsing
+    req = str(rawreq, "utf-8").split(' ')
+    print(req)
+    method = req[0]
+    (response, statusCode) = checkMethod(method)
+    body = ''
+    if not response:
+        uri = req[1]
+        body = getFiles(uri)
+        header ="""HTTP/1.1 {0} OK \
+Server: worstWebserverEver""".format(statusCode)
+    response = """{0} \
+{1}""".format(header, body)
+    data = str.encode(response, 'utf-8')
+    print("Data: {0}".format(data))
+    return data
+
+
+def checkMethod(method):
+    acceptedMethods = ["options", "head", "get"]
+    if method.lower() in acceptedMethods:
+        myMethod = method.upper()
+        statusCode = 200
+        if myMethod == 'OPTIONS':
+            response = "Allow: OPTIONS, HEAD, GET"
+        elif myMethod == 'HEAD':
+            response = True
+        elif myMethod == 'GET':
+            response = False
+    else:
+        statusCode = 503
+        response = "METHOD NOT AVAILABLE"
+    return response, statusCode
+
+
+def getFiles(uri):
+    if uri.split('/')[-1].split('.')[-1] != 'html':
+       uri = "{0}index.html".format(uri)
+       print(uri)
+    with open(".{0}".format(uri), 'rb') as newfile:
+        body = newfile.read()
+    return body
+
+
+if __name__ == '__main__':
+    print("You are running this package incorrectly. RTFM")
+    exit(1)
--- worstWebserverEver/server.py
+++ worstWebserverEver/server.py
@@ -6,19 +6,11 @@ def main():
     # TODO: add configuration import
     while True:
         (clientsocket, address) = serversocket.accept()
-        # mySocket(clientsocket)
-        rawreq = clientsocket.recv(1024)
-        req = str(rawreq, "utf-8").split(' ')
-        method = req[0]
-        uri = req[1]
-        addr = "{0}:{1}".format(address[0], address[1])
-        print("Connection from: {0} - {1}  --  {2}".format(addr, method, uri))
-        response = """\
-HTTP/1.1 200 OK
-
-Hello, World!
-"""
-        clientsocket.sendall(str.encode(response))
+        data = clientsocket.recv(1024)
+        import receive
+        response = receive.main(data)
+        print("This is my response: {0}".format(response))
+        clientsocket.sendall(response)
         clientsocket.close()
 
 
@@ -33,4 +25,5 @@ def createSocket():
 
 
 if __name__ == '__main__':
-    print("You are running this package incorrectly, rtfm.")
+    print("You are running this package incorrectly. RTFM.")
+    exit(1)
