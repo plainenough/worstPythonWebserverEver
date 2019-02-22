fde49f92c38704d1acd549584bcf95614bbef09c
Derrick Walton <dwalton@jane.com>
2019-02-22 14:33:20 -0700

    rework how headers are genereated

--- worstPythonWebserverEver/headers.py
+++ worstPythonWebserverEver/headers.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 
-def main(config, status):
+def main(config, statusCode):
     import time
     headers = []
+    statusMessage = {200: 'OK', 404: 'NotFound',
+                     500: 'InternalServerError',
+                     403: 'NotAuthorized',
+                     405: 'MethodNotAllowed'}
     timeFormat = 'Date: %a, %d %b %Y %H:%M:%S GMT'
-    headers.append("HTTP/1.1 {0} {1}".format(status.code, status.message))
+    headers.append("HTTP/1.1 {0} {1}".format(statusCode, statusMessage[statusCode]))
     headers.append("Server: worstPythonServerEver")
     headers.append("Location: {0}".format(config['server']['name']))
     headers.append(time.strftime(timeFormat, time.gmtime()))
