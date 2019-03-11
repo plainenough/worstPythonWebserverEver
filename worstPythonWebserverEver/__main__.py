7ee00c48b78b0da7497f6c67036af9037521e07f
Derrick Walton <dwalton@jane.com>
2019-03-11 12:08:04 -0600

    add except to catch ctrl-c

--- worstPythonWebserverEver/__main__.py
+++ worstPythonWebserverEver/__main__.py
@@ -3,8 +3,11 @@
 
 def main():
     import server as myServer
-    myServer.main()
-
+    import sys
+    try:
+        myServer.main()
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 if __name__ == '__main__':
     main()
