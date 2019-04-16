3d0621cc3136949cc3c74f427c330ca9c66897af
Derrick Walton <dwalton@jane.com>
2019-04-16 14:25:11 -0600

    add changes for multiprocessing

--- worstPythonWebserverEver/server.py
+++ worstPythonWebserverEver/server.py
@@ -7,32 +7,34 @@ def main():
     (args, config, log) = config.main()
     serversocket = createSocket(config)
     while True:
-        (clientsocket, address) = serversocket.accept()
-        data = clientsocket.recv(1024)
+        (csock, addr) = serversocket.accept()
+        #data = csock.recv(1024)
         p = mp.Process(target=handleRequest, args=(args, config,
-                                                   data, clientsocket))
+                                                   csock, addr))
         p.start()
         p.join()
+        print(mp.active_children())
     serversocket.close()
+    return
 
 
 def createSocket(config):
     import socket
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # serversocket.bind((socket.gethostname(), 80))
     serversocket.bind((config['server_ipv4'], config['server_port']))
-    # TODO: Wire up config here.
-    serversocket.listen(5)
+    serversocket.listen(1)
     return serversocket
 
 
-def handleRequest(args, config, data, clientsocket):
+def handleRequest(args, config, csock, addr):
     import receive
-    clientsocket.setblocking(0)
+    print("Connection from {0}".format(addr))
+    data = csock.recv(1024)
+    csock.setblocking(False)
     response = receive.main(args, config, data)
-    clientsocket.send(response)
-    clientsocket.close()
-    return
+    csock.sendall(response)
+    csock.close()
+    return False
 
 
 if __name__ == '__main__':
